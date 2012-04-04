9877639c5493a956b55dde75ad80a319b4e3e996
Philipp Spieß <philipp.spiess@myxcode.at>
2012-04-04 20:25:18 +0200

    Port 80, 'course

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ console.log(r)
 app.get('/', require('./routes/index'))
 app.get('/pharos(.min)?.js', require('./routes/pharos.js'))
 
-app.listen(3000, function() {
+app.listen(80, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
 });
